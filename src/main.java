817799957ef1a15e3a69e48ac688ecb668ad7a19
juan pablo gomez <Juangomezramirez527@gmil.com>
2022-06-30 16:24:59 -0500

    se modifico la clase

--- src/main.java
+++ src/main.java
@@ -1,9 +1,18 @@
 
+import java.util.Scanner;
+
+
 public class main {
 
-   
+   static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
-   
+       int num1,num2,suma;
+        System.out.println("Ingrese un numero :");
+        num1=entrada.nextInt();
+        System.out.println("Ingrese un numero :");
+        num2=entrada.nextInt();
+        suma=num1+num2;
+        System.out.println("La suma es: "+suma);
     }
     
 }
