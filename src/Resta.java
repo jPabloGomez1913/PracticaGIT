ba18d79aa5d51606734b1bf4deed1f9ed06cb6ac
juan pablo gomez <Juangomezramirez527@gmil.com>
2022-06-30 16:28:24 -0500

    clase resta

--- src/Resta.java
+++ src/Resta.java
@@ -1,4 +1,19 @@
 
 public class Resta {
+    int num1,num2;
+
+ 
+
+    public Resta(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
     
+    
+ 
+    public int resta(){
+    
+    
+        return num1 - num2;
+    }
 }
